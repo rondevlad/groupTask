8568ff275dd7df18801ea7fdc510fe2db1c93ceb
partap <partap@example.com>
2023-12-08 14:49:14 -0500

    s

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function plusSlides(n) {
   SlideShow(slidePosition += n);
 }
 
-//  images controls
+// images controls
 function currentSlide(n) {
   SlideShow(slidePosition = n);
 }
@@ -27,18 +27,21 @@ function SlideShow(n) {
   var i;
   var slides = document.getElementsByClassName("Containers");
   var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
+  if (n > slides.length) {
+    slidePosition = 1;
+  }
+  if (n < 1) {
+    slidePosition = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace(" enable", "");
+    circles[i].className = circles[i].className.replace(" enable", "");
   }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
-} 
-
+  slides[slidePosition - 1].style.display = "block";
+  circles[slidePosition - 1].className += " enable";
+}
 
 
 /* Cupid */
