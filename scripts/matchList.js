047737909fa52171894243dbbc22353c4f4d1a09
nahom <n01522132@humbermail.ca>
2023-12-13 00:32:13 -0500

    match list example

---
+++ scripts/matchList.js
@@ -0,0 +1,9 @@
+function toggleMoreText() {
+    var moreText = document.querySelector('.more-text');
+    moreText.style.display = (moreText.style.display === 'none' || moreText.style.display === '') ? 'block' : 'none';
+}
+
+function toggleMoreText2() {
+    var moreText2 = document.querySelector('.more-text2');
+    moreText2.style.display = (moreText2.style.display === 'none' || moreText2.style.display === '') ? 'block' : 'none';
+}
